4dea07e8473ca4a5997b5286b33998dd62abddea
ankitjain28may <ankitjain28may77@gmail.com>
2016-11-27 13:43:34 +0530

    Added hover and bg-colour on notices

--- content.js
+++ content.js
@@ -6,13 +6,16 @@ $(document).ready(function(){
             $(".notices img").remove();
             var content = $("li.modal-index", data);
             $.each(content, function( index, value ) {
-                var col = $("<div></div>").addClass("col-sm-12");
+                if(index%2==0)
+                    var col = $("<div></div>").addClass("col-sm-12 odd notice-data");
+                else
+                    var col = $("<div></div>").addClass("col-sm-12 notice-data");
                var notice = $("div.col", value).html();
                var t = $("<p></p>").text(notice);
                col.append(t);
                $(".notices").append(col);
             });
-            var col = $("<div class='text-center' ></div>").addClass("col-sm-12");
+            var col = $("<div class='text-center' ></div>").addClass("col-sm-12 link-col");
             var link = $("<a href='http://210.212.85.155/' class='link' target='_blank'></a>").text("Read more Notices");
             col.append(link);
             $(".notices").append(col);
